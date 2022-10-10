c92b641e87d8173473ca078ad484ccabc78c39a8
Morphage <4647675+Morphage@users.noreply.github.com>
2022-10-10 17:15:58 +0100

    Implement simple offers

--- src/main/scala/ShoppingCart.scala
+++ src/main/scala/ShoppingCart.scala
@@ -1,11 +1,25 @@
 import Products.prices
 
+import java.lang.Math.ceil
+
 case class ShoppingCart(private val items: List[String]) {
-  
+
   def add(item: String): ShoppingCart = ShoppingCart(items = items :+ item)
-  
-  def totalPrice: BigDecimal = items.map(prices).sum
-  
+
+  def totalPrice: BigDecimal =
+    items
+      .groupBy(identity)
+      .map { case (itemName, items) =>
+        val itemsRequiringPayment = ceil(
+          itemName match {
+            case "Apple" => items.size / 2.0           // buy one, get one free
+            case "Orange" => items.size / 3.0 * 2.0    // 3 for the price of 2
+          }
+        )
+        itemsRequiringPayment * prices(itemName)
+      }
+      .sum
+
 }
 
 object ShoppingCart {
